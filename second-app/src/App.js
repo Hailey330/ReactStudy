2acc89e688f072ef1d8c9d785b746f16c1ede4a5
baegopa <baegopa@wamwam.com>
2020-07-27 18:07:52 +0900

    router dom

--- second-app/src/App.js
+++ second-app/src/App.js
@@ -1,69 +1,21 @@
 import React, { Component } from 'react';
-import Post from './Post';
-import styled from 'styled-components';
-
-// * 스타일 컴포넌트는 class 바깥에 둔다.
-const ContainerBox = styled.div`
-  display: grid;
-  justify-content: center;
-`;
+import Nav from './Nav';
+import Home from './page/Home';
+import About from './page/About';
+import Profile from './page/Profile';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
-  state = {
-    posts: [
-      {
-        id: 1,
-        title: '제목1',
-      },
-      {
-        id: 2,
-        title: '제목2',
-      },
-      {
-        id: 3,
-        title: '제목3',
-      },
-    ],
-  };
-
   render() {
-    // 렌더링 될 때마다 계속 그려질 필요가 없기 때문에 const 사용
-    const add = () => {
-      this.setState({
-        posts: this.state.posts.concat({ id: 4, title: '제목4' }),
-      });
-    };
-
-    const del = () => {
-      this.setState({
-        posts: this.state.posts.filter((post) => {
-          return post.id !== 2;
-        }),
-      });
-    };
-
-    const update = () => {
-      const data = {
-        id: 2,
-        title: '제목200',
-      };
-
-      this.setState({
-        posts: this.state.posts.map((post) =>
-          post.id === 2 ? { ...post, ...data } : post
-        ),
-      });
-    };
-
     return (
-      <ContainerBox>
-        <button onClick={add}>추가</button>
-        <button onClick={del}>삭제</button>
-        <button onClick={update}>수정</button>
-        {this.state.posts.map((post) => {
-          return <Post id={post.id} title={post.title} />;
-        })}
-      </ContainerBox>
+      <div>
+        <Nav />
+        <br />
+        <br />
+        <Route path="/" component={Home} exact={true} />
+        <Route path="/about" component={About} />
+        <Route path="/profile" component={Profile} />
+      </div>
     );
   }
 }
